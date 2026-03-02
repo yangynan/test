8c167afd732441c4d9d5f36592de8c3f047e4d98
yangynan <y18043086211@163.com>
2026-03-02 15:33:04 +0800

    feat:测试推送到new branch在另一个项目拉取

---
+++ src/main/java/com/example/demo/DDD.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class DDD
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String n="Name";
+        System.out.println(printHello(n));
+    }
+
+    public static String printHello(String name) {
+        return name;
+    }
+}
